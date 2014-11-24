430d42543a2549571cdacb4ed057b8d15c305c81
kreolim <suspect91717@gmail.com>
2014-11-24 22:25:27 +0500

    Playing with fonts) A lot of hardcode

--- Calendar/Calendar/Page.cs
+++ Calendar/Calendar/Page.cs
@@ -28,35 +28,64 @@ namespace Calendar
 			{11, "November"},
 			{12, "December"}
 		};
+
+		private Dictionary<int, string> dayOfWeek = new Dictionary<int, string>()
+		{
+			{0, "MON"},
+			{1, "TUE"},
+			{2, "WED"},
+			{3, "THU"},
+			{4, "FRI"},
+			{5, "SAT"},
+			{6, "SUN"}
+		};
+
 		public Page(DateTime date)
 		{
 			data = new MonthFinder(date);
-			Height = 160;
-			Width = 160;
+			Height = 260;
+			Width = 260;
 			Paint += PaintDates;
 			Paint += PaintMonthYear;
+			Paint += PaintDaysOfWeek;
 			InitializeComponent();
+		}
+
+		private void PaintDaysOfWeek(object sender, PaintEventArgs e)
+		{
+			var graphics = e.Graphics;
+			var font = new Font(FontFamily.GenericMonospace, 10);
+			int offset = 34;
+			for (int i = 0; i < dayOfWeek.Count; i++)
+			{
+				graphics.DrawString(dayOfWeek[i], font, Brushes.LightCoral, 4 + i * offset, 35);
+			}
 		}
 
 		private void PaintMonthYear(object sender, PaintEventArgs e)
 		{
 			var graphics = e.Graphics;
-			graphics.DrawString(month[data.Month] + " " + data.Year.ToString(), DefaultFont, Brushes.Blue, 30, 4);
+			var f = new Font(FontFamily.GenericSansSerif, 16);
+			graphics.DrawString(month[data.Month] + "  " + data.Year.ToString(), f, Brushes.Blue, 4, 4);
 		}
 
 		void PaintDates(object sender, PaintEventArgs e)
 		{
 			var page = data.GetCalendarPage();
 			var x = 4;
-			var y = 20;
-			const int offset = 20;
+			var y = 55;
+			int offset = 34;
 			var graphics = e.Graphics;
+			var font = new Font(FontFamily.GenericSerif, 18);
 			for (int i = 0; i < page.Length; i++)
 			{
 				for (int j = 0; j < page[i].Length; j++)
 				{
+					var brush = Brushes.Black;
+					if (data.Day == page[i][j])
+						brush = Brushes.Red;
 					var toDraw = page[i][j] == 0 ? " " : page[i][j].ToString();
-					graphics.DrawString(toDraw, DefaultFont, Brushes.Black, x + offset*j, y + offset*i);
+					graphics.DrawString(toDraw, font, brush, x + offset*j, y + offset*i);
 				}
 			}
 		}
